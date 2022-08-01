48563d9cadc7192c34fe1e9893a73ab8eb1cc7c5
Marvin Blauth <marvin@slatanic.org>
2022-08-01 18:20:43 +0200

    Add info on JVM running the tool

--- src/main/java/JDump.java
+++ src/main/java/JDump.java
@@ -3,7 +3,16 @@ import dump.Dumps;
 public class JDump {
 
     public static void main(String[] args) {
+        showJVMInfo();
         Dumps.handle(args);
+    }
+
+    static void showJVMInfo() {
+        System.out.println("jdump running on " + System.getProperty("java.vm.vendor") + " " +
+                System.getProperty("java.vm.name") + " " +
+                System.getProperty("java.vm.version") + " " +
+                System.getProperty("os.arch")
+        );
     }
 
 }
