af3754f5652248a3364f869e2c155b0e1f1e2e0b
Marvin Blauth <marvin@slatanic.org>
2022-08-01 20:04:00 +0200

    Don't dump data for the JVM running jdump

--- src/main/java/dump/Dump.java
+++ src/main/java/dump/Dump.java
@@ -4,9 +4,13 @@ import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
 
 public abstract class Dump {
+    private final long currentPid = ProcessHandle.current().pid();
+
     abstract void performFor(VirtualMachineDescriptor vmd);
 
     void performForAll() {
-        VirtualMachine.list().forEach(this::performFor);
+        VirtualMachine.list().stream()
+                .filter(vmd -> currentPid != Long.parseLong(vmd.id())) // we don't want to work with our own VM
+                .forEach(this::performFor);
     }
 }
